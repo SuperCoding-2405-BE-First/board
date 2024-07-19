a933ff920134e658ad7a271357bd82cff06858f5
HwangSeungMin <vlglol@naver.com>
2024-07-19 10:48:13 +0900

    doc: swagger 안내문구 작성

--- src/main/java/com/supercoding/first/projectBE/controller/GreatController.java
+++ src/main/java/com/supercoding/first/projectBE/controller/GreatController.java
@@ -5,6 +5,8 @@ import com.supercoding.first.projectBE.config.jwt.TokenProvider;
 import com.supercoding.first.projectBE.dto.GreatRequest;
 import com.supercoding.first.projectBE.entity.Great;
 import com.supercoding.first.projectBE.service.GreatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +23,9 @@ public class GreatController {
 
     private final GreatService greatService;
     private final TokenProvider tokenProvider;
-
-    @GetMapping("/great/check/{post_id}") // 좋아요 여부 확인
+    @Operation(summary = "좋아요 확인", description = "게시물에 대한 좋아요 여부 확인")
+    @ApiResponse(responseCode = "200", description = "true / false 반환")
+    @GetMapping("/great/check/{post_id}")
     public ResponseEntity<Boolean> greatCheck(@PathVariable Long post_id,@RequestHeader("Authorization") String token ){
         String jwtToken = token.substring(7);// 토큰 값에서 'Bearer ' 부분을 제거
 
@@ -35,7 +38,9 @@ public class GreatController {
         return ResponseEntity.ok(greatService.greatCheck(post_id,userId));
     }
 
-    @PostMapping("/great") // 좋아요 추가 (표시)
+    @Operation(summary = "좋아요 설정(추가)", description = "게시물에 대한 좋아요 설정(추가)")
+    @ApiResponse(responseCode = "200", description = "생성 객체 및 , 201 생성코드 반환")
+    @PostMapping("/great")
     public ResponseEntity<Great> createGreat(@RequestBody GreatRequest greatRequest, @RequestHeader("Authorization") String token){
         String jwtToken = token.substring(7);// 토큰 값에서 'Bearer ' 부분을 제거
 
@@ -49,7 +54,9 @@ public class GreatController {
         return new ResponseEntity<>(great,HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/great/{post_id}") // 좋아요 삭제 (해제)
+    @Operation(summary = "좋아요 해제(삭제)", description = "게시물에 대한 좋아요 해제(삭제)")
+    @ApiResponse(responseCode = "204", description = "204 No Content 상태 반환")
+    @DeleteMapping("/great/{post_id}")
     public ResponseEntity<Void> deletePost(@PathVariable Long post_id, @RequestHeader("Authorization") String token) {
         String jwtToken = token.substring(7);// 토큰 값에서 'Bearer ' 부분을 제거
 
@@ -65,7 +72,9 @@ public class GreatController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping("/great/count/{post_id}") // 좋아요 카운트 확인
+    @Operation(summary = "좋아요 수 확인", description = "게시물에 대한 좋아요 개수 확인")
+    @ApiResponse(responseCode = "200", description = "좋아요 수 반환")
+    @GetMapping("/great/count/{post_id}")
     public ResponseEntity<Long> getPostGreatCount(@PathVariable Long post_id, @RequestHeader("Authorization") String token){
         String jwtToken = token.substring(7);// 토큰 값에서 'Bearer ' 부분을 제거
 
--- src/main/java/com/supercoding/first/projectBE/controller/PostController.java
+++ src/main/java/com/supercoding/first/projectBE/controller/PostController.java
@@ -9,6 +9,8 @@ import com.supercoding.first.projectBE.entity.Post;
 import com.supercoding.first.projectBE.exception.PostNotFoundException;
 import com.supercoding.first.projectBE.repository.PostRepository;
 import com.supercoding.first.projectBE.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,20 +29,8 @@ public class PostController {
     private final PostRepository postRepository;
     private final TokenProvider tokenProvider;
 
-    // 게시물 전체 조회
-    @GetMapping("/posts")
-    public ResponseEntity<Map<String,List<PostResponse>>> getAllPosts(@RequestHeader("Authorization") String token){ // API 조회 예시에 맞게 맵에 리스트를 담아 리턴 하도록 수정
-        String accessToken = tokenProvider.getAccessToken(token);
-
-        if(!tokenProvider.validToken(accessToken)){
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        List<PostResponse> posts = postService.getAllPosts();
-        Map postsMap = new HashMap();
-        postsMap.put("posts",posts);
-        return ResponseEntity.ok().body(postsMap);
-    }
-    // 게시물 추가 API
+    @Operation(summary = "게시물 생성", description = "제목 및 내용을 받아 인증된 사용자의 게시물 생성")
+    @ApiResponse(responseCode = "200", description = "게시물이 성공적으로 작성 되었습니다 메세지 출력")
     @PostMapping("/posts")
     public ResponseEntity<Map> createPost(@RequestBody PostRequest requestDto,@RequestHeader("Authorization") String token) {
         // 토큰 값에서 'Bearer ' 부분을 제거
@@ -58,7 +48,24 @@ public class PostController {
     }
 
 
-    // 게시물 상세 조회 API
+    @Operation(summary = "게시물 전체 조회", description = "전체 게시물 조회")
+    @ApiResponse(responseCode = "200", description = "게시물 리스트 전체 리스트 조회")
+    @GetMapping("/posts")
+    public ResponseEntity<Map<String,List<PostResponse>>> getAllPosts(@RequestHeader("Authorization") String token){ // API 조회 예시에 맞게 맵에 리스트를 담아 리턴 하도록 수정
+        String accessToken = tokenProvider.getAccessToken(token);
+
+        if(!tokenProvider.validToken(accessToken)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        List<PostResponse> posts = postService.getAllPosts();
+        Map postsMap = new HashMap();
+        postsMap.put("posts",posts);
+        return ResponseEntity.ok().body(postsMap);
+    }
+
+
+    @Operation(summary = "게시물 상세 조회", description = "게시물 아이디의 게시물 조회")
+    @ApiResponse(responseCode = "200", description = "게시물 아이디의 게시물 조회")
     @GetMapping("/posts/{post_id}")
     public ResponseEntity<PostResponse> getPostById(@PathVariable Long post_id,@RequestHeader("Authorization") String token) {
         Post post = postService.getPostById(post_id);
@@ -68,7 +75,8 @@ public class PostController {
         return ResponseEntity.ok().body(new PostResponse(post));
     }
 
-    // 게시물 수정 API (제목과 내용만 수정 가능)
+    @Operation(summary = "게시물 수정", description = "게시물 아이디의 게시물 수정")
+    @ApiResponse(responseCode = "200", description = "수정된 값 반환")
     @PutMapping("/posts/{post_id}")
     public ResponseEntity<PostResponse> updatePost(
             @PathVariable Long post_id,
@@ -82,7 +90,8 @@ public class PostController {
         return ResponseEntity.ok().body(new PostResponse(updatedPost));
     }
 
-    // 게시물 삭제 API
+    @Operation(summary = "게시물 삭제", description = "게시물 아이디 기준 게시물 삭제")
+    @ApiResponse(responseCode = "200", description = "게시물 삭제 메세지 출력")
     @DeleteMapping("/posts/{post_id}")
     public ResponseEntity<Map> deletePost(@PathVariable Long post_id,@RequestHeader("Authorization") String token) {
         String accessToken = tokenProvider.getAccessToken(token);
@@ -100,7 +109,8 @@ public class PostController {
         return ResponseEntity.ok().body(map);
     }
 
-    // 게시물 검색 : 작성자 이메일 API
+    @Operation(summary = "게시물 검색 (author_email)", description = "작성자 메일을 입력받아 해당 게시물 조회")
+    @ApiResponse(responseCode = "200", description = "검색어 메일의 작성자 게시물 리스트 조회 ")
     @GetMapping("/posts/search")
     public ResponseEntity<List<PostResponse>> getPostsByEmail(@RequestParam(name="author_email") String email, @RequestHeader("Authorization") String token) {
         String accessToken = tokenProvider.getAccessToken(token);
